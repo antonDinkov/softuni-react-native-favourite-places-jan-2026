e8c81194e5971abaf0d3e0062238dbdbb94e600c
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-02-04 18:43:58 +0200

    Add json server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
 
 import AppNavigation from './navigation/AppNavigation.jsx';
 import { NavigationContainer } from '@react-navigation/native';
